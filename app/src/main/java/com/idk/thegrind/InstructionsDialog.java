eee365a5a546cfed00638a55c3b3b5c8d1759982
Catherine Zhu <catherinepaopao@gmail.com>
2022-08-25 21:10:51 -0700

    Added instructions dialog

---
+++ app/src/main/java/com/idk/thegrind/InstructionsDialog.java
@@ -0,0 +1,28 @@
+package com.idk.thegrind;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+
+public class InstructionsDialog extends Dialog {
+    public InstructionsDialog(@NonNull Context context) {
+        super(context);
+        setContentView(R.layout.instructions_dialog);
+    }
+
+    public void showDialog(){
+        Button close = findViewById(R.id.close_instruct);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        show();
+    }
+}
--- app/src/main/java/com/idk/thegrind/MainActivity.java
+++ app/src/main/java/com/idk/thegrind/MainActivity.java
@@ -2,7 +2,9 @@ package com.idk.thegrind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,19 +12,34 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button start;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs = this.getSharedPreferences("grind_prefs", Context.MODE_PRIVATE);
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
 
-        start = findViewById(R.id.start);
+        start = findViewById(R.id.startbutton);
+        InstructionsDialog instructDialog = new InstructionsDialog(this);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(prefs.getBoolean("isNew", true)){
+                    instructDialog.showDialog();
+
+                    SharedPreferences.Editor edit = prefs.edit();
+                    edit.putBoolean("isNew", false);
+                    edit.apply();
+                } else {
+                    SharedPreferences.Editor edit = prefs.edit();
+                    edit.putBoolean("isNew", true);
+                    edit.apply();
+                }
+
                 /* Sounds.play(MainActivity.this, R.raw.select);
                 Intent toQuestions = new Intent(getApplicationContext(), QuestionsPage.class);
                 startActivity(QuestionsPage); */
