fb5b510773f01fbd70ee8ab27936e69f701ed3e1
Catherine Zhu <catherinepaopao@gmail.com>
2022-08-26 14:03:15 -0700

    settings

--- app/src/main/java/com/idk/thegrind/GamePage.java
+++ app/src/main/java/com/idk/thegrind/GamePage.java
@@ -1,23 +1,36 @@
 package com.idk.thegrind;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class GamePage extends AppCompatActivity {
-    RecyclerView questions;
+    SharedPreferences prefs;
     Button leaderboardButton;
     Button settingsButton;
     Button questionsButton;
     TextView title;
 
+    RecyclerView questionsQuestions;
+    Button instructionsSettings;
+    Button profileSettings;
+    Button subjectSettings;
+    Switch sfxSettings;
+    TextView generalSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs = this.getSharedPreferences("grind_prefs", Context.MODE_PRIVATE);
         setContentView(R.layout.game_page);
 
         getSupportActionBar().hide();
@@ -25,8 +38,21 @@ public class GamePage extends AppCompatActivity {
         leaderboardButton = findViewById(R.id.leaderboard_button);
         settingsButton = findViewById(R.id.settings_button);
         questionsButton = findViewById(R.id.question_button);
-        questions = findViewById(R.id.questions);
+        questionsQuestions = findViewById(R.id.questions);
         title = findViewById(R.id.title);
+        instructionsSettings = findViewById(R.id.instructions_settings);
+        profileSettings = findViewById(R.id.profile_button);
+        subjectSettings = findViewById(R.id.subjects_button);
+        sfxSettings = findViewById(R.id.sfx_switch);
+        generalSettings = findViewById(R.id.general);
+
+        if(prefs.getBoolean("isSFX", true)){
+            sfxSettings.setChecked(true);
+        }
+
+        hideLeaderboard();
+        hideSettings();
+        showQuestions();
 
         questionsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +60,9 @@ public class GamePage extends AppCompatActivity {
                 if(GameData.currentPage != 1) {
                     GameData.currentPage = 1;
                     title.setText(R.string.daily_qs);
-                    questions.setVisibility(View.VISIBLE);
+                    showQuestions();
+                    hideSettings();
+                    hideLeaderboard();
                 }
             }
         });
@@ -45,7 +73,9 @@ public class GamePage extends AppCompatActivity {
                 if(GameData.currentPage != 2) {
                     GameData.currentPage = 2;
                     title.setText(R.string.overall_lb);
-                    questions.setVisibility(View.GONE);
+                    hideQuestions();
+                    hideSettings();
+                    showLeaderboard();
                 }
             }
         });
@@ -56,9 +86,73 @@ public class GamePage extends AppCompatActivity {
                 if(GameData.currentPage != 3) {
                     GameData.currentPage = 3;
                     title.setText(R.string.settings);
-                    questions.setVisibility(View.GONE);
+                    hideQuestions();
+                    showSettings();
+                    hideLeaderboard();
                 }
             }
         });
+
+        sfxSettings.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor edit = prefs.edit();
+
+                if(!isChecked){
+                    edit.putBoolean("isSFX", false);
+                } else {
+                    edit.putBoolean("isSFX", true);
+                }
+                edit.apply();
+            }
+        });
+
+        instructionsSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InstructionsDialog instructDialog = new InstructionsDialog(GamePage.this);
+                instructDialog.showDialog();
+            }
+        });
+
+        profileSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProfileDialog profileDialog = new ProfileDialog(GamePage.this);
+                profileDialog.showDialog();
+            }
+        });
+    }
+
+    public void hideQuestions(){
+        questionsQuestions.setVisibility(View.GONE);
+    }
+
+    public void showQuestions(){
+        questionsQuestions.setVisibility(View.VISIBLE);
+    }
+
+    public void hideSettings(){
+        instructionsSettings.setVisibility(View.GONE);
+        profileSettings.setVisibility(View.GONE);
+        sfxSettings.setVisibility(View.GONE);
+        subjectSettings.setVisibility(View.GONE);
+        generalSettings.setVisibility(View.GONE);
+    }
+
+    public void showSettings(){
+        instructionsSettings.setVisibility(View.VISIBLE);
+        profileSettings.setVisibility(View.VISIBLE);
+        sfxSettings.setVisibility(View.VISIBLE);
+        subjectSettings.setVisibility(View.VISIBLE);
+        generalSettings.setVisibility(View.VISIBLE);
+    }
+
+    public void hideLeaderboard(){
+
+    }
+
+    public void showLeaderboard(){
+
     }
 }
---
+++ app/src/main/java/com/idk/thegrind/ProfileDialog.java
@@ -0,0 +1,40 @@
+package com.idk.thegrind;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+
+public class ProfileDialog extends Dialog {
+    SharedPreferences prefs;
+
+    public ProfileDialog(@NonNull Context context) {
+        super(context);
+        setContentView(R.layout.profile_dialog);
+
+        prefs = context.getSharedPreferences("grind_prefs", Context.MODE_PRIVATE);
+    }
+
+    public void showDialog(){
+        Button save = findViewById(R.id.save);
+        EditText nameEntry = findViewById(R.id.pref_name);
+        nameEntry.setText(prefs.getString("prefName", ""));
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor edit = prefs.edit();
+                edit.putString("prefName", nameEntry.getText().toString());
+                edit.apply();
+                dismiss();
+            }
+        });
+
+        show();
+    }
+
+}
